9e576e44fddddcb620c10db3751b2c8158fa1ccf
Jaz <jwhite@launchdarkly.com>
2024-01-29 12:57:54 -0500

    add basic job summary action

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,11 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const summary: string[] = core.getMultilineInput('summary')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    for (const line of summary) {
+      core.summary.addRaw(line)
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
